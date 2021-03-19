db6fe017a1ca5961e4ec26ad1833a93552bc68b1
Pridemore <pridemoreviriri@gmail.com>
2021-03-19 07:12:06 +0200

    creating api,domain,commons,persistence and service modules for the backend development.

---
+++ domain/src/test/java/com/santo/vms/domain/DomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santo.vms.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
